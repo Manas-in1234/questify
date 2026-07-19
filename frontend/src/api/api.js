00831605d94b68fa1b4f8f9eabe85e97ff0bc7a0
Manas <kassetymanas123@gmail.com>
2026-07-19 15:17:39 +0530

    Connect frontend to deploy backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://questify-backend-5w28.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
